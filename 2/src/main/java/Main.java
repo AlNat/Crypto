ed87cb40dbc4c02a6ce6a2514bb0fea6b998d2cb
AlNat <aleksnatarov@gmail.com>
2016-10-01 08:43:06 +0300

    2 passed

--- 2/src/main/java/Main.java
+++ 2/src/main/java/Main.java
@@ -16,7 +16,10 @@ public class Main {
         RW rw = new RW();
 
         String path1 = "C:\\Users\\AlNat\\Source\\Studi\\out1.txt"; //args[0];
-        final int size = 40000; // Integer.valueOf(args[1]);
+        final int size = 1000000; // 100 млн - работает ок. А вот на млрд - ошиба при записи - кучи не хватает
+        // Эксперементальным путем было выясненно, что максимально переваримый обьем на 32 разрадяной JVM - ~400
+        // 100000; // Integer.valueOf(args[1]);
+        //Integer.MAX_VALUE // - ошибка в выделении памяти для кучи
 
         rw.Calc (path1, size);
 
