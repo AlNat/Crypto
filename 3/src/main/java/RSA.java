909d4ab9141e8b1004c4bb74014314d4104a30c5
AlNat <aleksnatarov@gmail.com>
2016-10-03 20:08:51 +0300

    3 complete

--- 3/src/main/java/RSA.java
+++ 3/src/main/java/RSA.java
@@ -5,14 +5,12 @@ import java.security.SecureRandom;
  * Created by @author AlNat on 26.09.2016.
  * Licensed by Apache License, Version 2.0
  */
-public class RSA {
+class RSA {
 
     private BigInteger n, d, e;
 
-    private int bitlen = 1024;
-
     /**
-     * Create an instance that can encrypt using someone elses public key.
+     * Создаем с уже имеющимеся n и e
      */
     RSA(BigInteger newn, BigInteger newe) {
         n = newn;
@@ -20,16 +18,18 @@ public class RSA {
     }
 
     /**
-     * Create an instance that can both encrypt and decrypt.
+     * Создаем числа рандомно
      */
     RSA(int bits) {
-        bitlen = bits;
+
+        // Создаем p и q рандомно, используя длинну в битах
         SecureRandom r = new SecureRandom();
-        BigInteger p = new BigInteger(bitlen / 2, 100, r);
-        BigInteger q = new BigInteger(bitlen / 2, 100, r);
+        BigInteger p = new BigInteger(bits / 2, 100, r);
+        BigInteger q = new BigInteger(bits / 2, 100, r);
+
+        // Делаем остальное по формулам
         n = p.multiply(q);
-        BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q
-                .subtract(BigInteger.ONE));
+        BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
         e = new BigInteger("3");
         while (m.gcd(e).intValue() > 1) {
             e = e.add(new BigInteger("2"));
@@ -38,45 +38,32 @@ public class RSA {
     }
 
     /**
-     * Encrypt the given plaintext message.
+     * Шифрует сообщение
      */
-    synchronized String encrypt(String message) {
-        return (new BigInteger(message.getBytes())).modPow(e, n).toString();
-    }
-
-    /**
-     * Encrypt the given plaintext message.
-     */
-    synchronized BigInteger encrypt(BigInteger message) {
+    BigInteger encrypt(BigInteger message) {
         return message.modPow(e, n);
     }
 
-    /**
-     * Decrypt the given ciphertext message.
-     */
-    synchronized String decrypt(String message) {
-        return new String((new BigInteger(message)).modPow(d, n).toByteArray());
-    }
 
     /**
-     * Decrypt the given ciphertext message.
+     * Расшифровывает сообщение
      */
-    synchronized BigInteger decrypt(BigInteger message) {
+    BigInteger decrypt(BigInteger message) {
         return message.modPow(d, n);
     }
 
 
     /**
-     * Return the modulus.
+     * Получение n
      */
-    synchronized BigInteger getN() {
+    BigInteger getN() {
         return n;
     }
 
     /**
-     * Return the public key.
+     * Получение e
      */
-    synchronized BigInteger getE() {
+    BigInteger getE() {
         return e;
     }
 
